41e81d5a6d3e02f05b392cd0c1e34c2c113e8604
Jordan Buckmaster <buckmast@purdue.edu>
2019-12-30 21:59:09 -0600

    Add Realm database to project

--- App.js
+++ App.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
+import Realm from 'realm'
 import Setup from "./src/boot/Setup";
+import { Text } from "react-native";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { realm: null };
+  }
+
+  componentDidMount() {
+    Realm.open({
+      schema: [{name: 'Dog', properties: {name: 'string'}}]
+    }).then(realm => {
+      realm.write(() => {
+        realm.create('Dog', {name: 'Rex'});
+      });
+      this.setState({ realm });
+    });
+  }
+
+  componentWillUnmount() {
+    // Close the realm if there is one open.
+    const {realm} = this.state;
+    if (realm !== null && !realm.isClosed) {
+      realm.close();
+    }
+  }
+
   render() {
-    return <Setup />;
+    const info = this.state.realm
+      ? 'Number of dogs in this Realm: ' + this.state.realm.objects('Dog').length
+      : 'Loading...';
+
+    return ( 
+      <>
+        <Text>{info}</Text>
+        <Setup />
+      </>);
   }
 }
