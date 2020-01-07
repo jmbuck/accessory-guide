56f755c8629dc3cc93120e2c9cddebbd4eb3586c
Jordan Buckmaster <buckmast@purdue.edu>
2020-01-06 19:23:09 -0600

    Fix bug in form inputs

--- src/components/GenerateForm.js
+++ src/components/GenerateForm.js
@@ -17,15 +17,15 @@ export default class GenerateForm extends Component {
 
   isErrored = () => {
     const { errors } = this.state
-    for(error in Object.values(errors)) {
+    for(let error of Object.values(errors)) {
       if(error) return true
     }
     return false
   }
 
-  onChange = (text, errorKey, type) => {
+  onChange = (text, key, type) => {
     // Check if text is a number
-    const { errors } = this.state
+    const { errors, values } = this.state
     let reg = /^\d+$/;
 
     if(reg.test(text)) {
@@ -35,78 +35,75 @@ export default class GenerateForm extends Component {
         // Min Reps
         if(num >= 10 && num <= 200) {
           // Number is in a valid range
-          errors[errorKey] = null
+          errors[key] = null
         } else if(num < 10) {
-          errors[errorKey] = 'Minimum reps should be 10 or more'
+          errors[key] = 'Minimum reps should be 10 or more'
         } else {
-          errors[errorKey] = 'Minimum reps should be 200 or fewer'
+          errors[key] = 'Minimum reps should be 200 or fewer'
         }
       } else {
         // # of Exercises
         if(num >= 1 && num <= 5) {
           // Number is in a valid range
-          errors[errorKey] = null 
+          errors[key] = null 
         } else if(num < 1) {
-          errors[errorKey] = '# of exercises should be 1 or more'
+          errors[key] = '# of exercises should be 1 or more'
         } else {
-          errors[errorKey] = '# of exercies should be 5 or fewer'
+          errors[key] = '# of exercies should be 5 or fewer'
         }
       }
     } else {
       //Text is not a number
       if(text.length > 0) {
-        errors[errorKey] = 'Must be a number'
+        errors[key] = 'Must be a number'
       } else {
-        errors[errorKey] = 'Field cannot be empty'
+        errors[key] = 'Field cannot be empty'
       }   
     }
 
-    this.setState({ errors, isErrored: this.isErrored() })
+    values[key] = text
+    this.setState({ errors, values, isErrored: this.isErrored() })
   }
   
   handleSubmit = (ev) => {
-    ['Pull', 'Push', 'Legs', 'Core']
-    .forEach((name) => {
-      let valueReps = this[name + 'MinRepsRef'].value();
-      let valueExercises = this[name + 'NumExercisesRef'].value();
-
-      console.log(valueReps)
-      console.log(valueExercises)
-    });
+    // TODO: Generate a workout, update database
   }
 
-  renderFormSection = (sectionHeader, defaultReps, defaultExercies) => {
-    let { errors } = this.state
-    const minRepsErrorKey = sectionHeader + 'MinReps'
-    const numExercisesErrorKey = sectionHeader + 'NumExercises'
+  renderFormSection = (sectionHeader, defaultReps, defaultExercises) => {
+    let { errors, values } = this.state
+    const minRepsKey = sectionHeader + 'MinReps'
+    const numExercisesKey = sectionHeader + 'NumExercises'
+
+    if(!values[minRepsKey]) values[minRepsKey] = defaultReps.toString()
+    if(!values[numExercisesKey]) values[numExercisesKey] = defaultExercises.toString()
     
     return (
        <View>         
          <Text>{sectionHeader}</Text>  
          <TextField 
-           ref={this[sectionHeader + 'MinRepsRef']}
            label="Min Reps"
            textColor={offBlack}
            activeLineWidth={1}
            tintColor={accent}
            baseColor={lightOffBlack}
            errorColor={error}
-           error={errors[minRepsErrorKey]}
+           error={errors[minRepsKey]}
            defaultValue={defaultReps.toString()}
-           onChangeText={(text) => this.onChange(text, minRepsErrorKey, 'reps')}
+           value={values[minRepsKey]}
+           onChangeText={(text) => this.onChange(text, minRepsKey, 'reps')}
          />
  
          <TextField 
-           ref={this[sectionHeader + 'NumExercisesRef']}
            label="# of Exercises"
            textColor={offBlack}
            activeLineWidth={1}
            tintColor={accent}
            baseColor={lightOffBlack}
            errorColor={error}
-           error={errors[numExercisesErrorKey]}
-           defaultValue={defaultExercies.toString()}
-           onChangeText={(text) => this.onChange(text, numExercisesErrorKey, 'exercises')}
+           error={errors[numExercisesKey]}
+           defaultValue={defaultExercises.toString()}
+           value={values[numExercisesKey]}
+           onChangeText={(text) => this.onChange(text, numExercisesKey, 'exercises')}
          />
        </View>
      )
